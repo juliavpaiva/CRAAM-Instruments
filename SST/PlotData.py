891ff1b00e2bbcf543fbe42be0b5c68ba324ae42
Bruno Mortella <bmortella@gmail.com>
2018-10-22 10:30:48 -0300

    Forgot to make some changes

--- SST/PlotData.py
+++ SST/PlotData.py
@@ -11,13 +11,13 @@ def getTimeAxis(d):
     day   = int(d.MetaData['ISODate'][8:])
     for i in np.arange(ndata):
         ms = d.Data['time'][i]
-        hours =  ms / 36000000
-        minutes = (ms % 36000000) / 600000
+        hours =  ms // 36000000
+        minutes = (ms % 36000000) // 600000
         seconds = ((ms % 36000000) % 600000) / 1.0E+04
         seconds_int  = int(seconds)
         seconds_frac = seconds - int(seconds)
         useconds     = int(seconds_frac * 1e6)
-        ssttime[i] = dt.datetime(int(year),int(month),int(day),int(hours),int(minutes),seconds_int,useconds)
+        ssttime[i] = dt.datetime(year,month,day,hours,minutes,seconds_int,useconds)
                         
 
     return ssttime
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
     try:
         d.readRBDinDictionary(RBDfname)
-    except Exception as e:
+    except Exception:
         print("An unexpected exception occurred")
         sys.exit(1)
 
